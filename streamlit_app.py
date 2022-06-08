f675b40a810c8a638fb51d49338cf32ea7a14c88
Shreyanew <107099816+Shreyanew@users.noreply.github.com>
2022-06-08 12:08:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,25 @@
 import streamlit
 
-streamlit.title('My parents new healthy dinner')
+import pandas
+
+streamlit.title('My parents new healthy dinner');
+
+streamlit.header('Breakfast Menu');
+streamlit.text('🥣 Omega 3 & blueberry oatmeal');
+streamlit.text(' 🥗 Kale, spinach & rocket smothie');
+streamlit.text('🐔 Hard-boiled Free-range egg');
+streamlit.text('🥑🍞 Avacado toast');
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+my_fruit_list=my_fruit_list.set_index('Fruit')
+#streamlit.multiselect("Pick some fruit:" , list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some fruit:" , list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show);
+
+#create a repeatable code block(called function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+  fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
